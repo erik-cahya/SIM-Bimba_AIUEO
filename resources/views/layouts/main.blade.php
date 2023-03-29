ea8b7bc07d2a13694e1d9b80fd77ccb238b71829
erik-cahya <erik.cahya841@gmail.com>
2023-03-29 17:43:51 +0800

    update code

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,12 +16,14 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- core:css -->
     <link rel="stylesheet" href="{{ asset('vendors/core/core.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendors/datatables.net-bs5/dataTables.bootstrap5.css') }}">
     <link rel="stylesheet" href="{{ asset('fonts/feather-font/css/iconfont.css') }}">
     <link rel="stylesheet" href="{{ asset('vendors/flag-icon-css/css/flag-icon.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
-    {{-- <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" /> --}}
+    <link rel="stylesheet" href="{{ asset('css/demo1/style.css') }}">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" />
 </head>
 
 <body>
@@ -32,8 +34,11 @@
 
     <!-- core:js -->
     <script src="{{ asset('vendors/core/core.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-bs5/dataTables.bootstrap5.js') }}"></script>
     <script src="{{ asset('vendors/feather-icons/feather.min.js') }}"></script>
     <script src="{{ asset('js/template.js') }}"></script>
+    <script src="{{ asset('js/data-table.js') }}"></script>
 
 
 </body>
