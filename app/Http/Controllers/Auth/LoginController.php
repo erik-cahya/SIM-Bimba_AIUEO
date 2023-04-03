b399d3e60f8f545f08b1c65c8b1438643dd0297e
erik-cahya <erik.cahya841@gmail.com>
2023-04-04 01:36:09 +0800

    build: add model and murid counter

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,11 +4,33 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class LoginController extends Controller
 {
     public function index()
     {
         return view('auth.login.index');
+    }
+
+    public function authenticate(Request $request)
+    {
+        // $credentials = $request->validate([
+        //     'email' => 'required|email',
+        //     'password' => 'required'
+        // ]);
+
+        // if (Auth::attempt($credentials)) {
+        //     $request->session()->regenerate();
+        //     dd(auth());
+        //     return redirect()->intended('/dashboard');
+
+        // }
+        // return back()->with('loginError', 'Login Failed');
+
+        $data['auth'] = 'guru';
+        return view('home', $data);
     }
 }
