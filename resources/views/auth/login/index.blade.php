e16600f52863047dbe2d0f2f713a491c83dabe8b
erik-cahya <erik.cahya841@gmail.com>
2023-04-19 17:05:34 +0800

    UI: ubah tampilan login

--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -13,15 +13,15 @@
                                 <div class="auth-form-wrapper d-flex flex-column align-items-center justify-content-center px-5 py-2"
                                     style="height: 400px;">
                                     <a href="#" class="noble-ui-logo d-block mb-2">
-                                        <img src="{{ asset('images/logo.png') }}" width="44px" alt="">
-                                        Bimba <span>A I U E O</span>
+                                        <img src="{{ asset('images/logo.png') }}" width="100px" alt="">
+                                        {{-- Bimba <span>A I U E O</span> --}}
                                     </a>
                                     <form class="forms-sample mt-4" method="GET" action="{{ route('dashboard') }}"
                                         style="width: 600px;">
                                         @csrf
                                         <div class="mb-3">
                                             <input type="email" class="form-control" id="email" name="email"
-                                                placeholder="Masukkan Email">
+                                                placeholder="Masukkan Username">
                                         </div>
                                         <div class="mb-3">
                                             <input type="password" class="form-control" id="password" name="password"
