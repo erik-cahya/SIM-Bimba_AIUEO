d871ebbe24decd90ca6f9923b5f4e29804af3991
erik-cahya <erik.cahya841@gmail.com>
2023-05-02 02:26:54 +0800

    update: auth guru fitur pekembangan dan tambah perkembangan fix

--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="page-wrapper full-page">
         <div class="page-content d-flex align-items-center justify-content-center">
-            <div class="row w-50 ">
-                <div class="col-md-8 col-xl-6 mx-auto">
+            <div class="row">
+                <div class="mx-auto">
                     <div class="card" style=" width:650px; height:400px">
                         <div class="row justify-content-center">
 
